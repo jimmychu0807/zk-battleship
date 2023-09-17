c3584591a70cbdc084085d9e194f44cba2f90a17
Jimmy Chu <jimmychu0807@gmail.com>
2023-09-17 20:19:14 +0800

    Allow ship drag and drop (#7)
    * Added shipPlate
    
    * displaying ships
    
    * updated
    
    * prettified
    
    * updated
    
    * updated
    
    * updated
    
    * Fix on github action
    
    * updated
    
    * updated
    
    * updated
    
    * updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,33 @@
 import { Stage, Text } from "@pixi/react";
-import BoardGrid from "./BoardGrid";
 
-import "./App.css";
 import { HeadingTextStyle, GRID_COLS, GRID_SIZE } from "./consts";
+import BoardGrid from "./BoardGrid";
+import ShipPlate from "./ShipPlate";
 
 function App() {
+  let y = 0;
+
   return (
     <Stage
       options={{
         antialias: true,
         autoDensity: true,
         backgroundColor: 0xeef1f5,
+        eventMode: "static",
+        resizeTo: window,
       }}
+      width={window.innerWidth}
+      height={window.innerHeight}
     >
-      <Text text="Your board" x={0} y={0} style={HeadingTextStyle} />
-      <BoardGrid x={0} y={50} />
+      <Text text="Your board" x={0} y={y} style={HeadingTextStyle} />
+      <BoardGrid x={0} y={(y += 50)} />
       <Text
         text="Your ships (pick 5 below):"
         x={0}
-        y={50 + GRID_COLS * GRID_SIZE + 10}
+        y={(y += GRID_COLS * GRID_SIZE + 10)}
         style={HeadingTextStyle}
       />
+      <ShipPlate x={0} y={(y += 50)} />
     </Stage>
   );
 }
--- frontend/src/ShipSprites.tsx
+++ frontend/src/ShipSprites.tsx
@@ -1,39 +1,72 @@
-import { Sprite } from "@pixi/react";
+import { useRef } from "react";
+import { Sprite, useApp } from "@pixi/react";
 import * as PIXI from "pixi.js";
 
-import { GRID_SIZE } from "./consts";
+import { GRID_SIZE, SHIPS } from "./consts";
 
 interface ShipSpritesProps {
-  ship: keyof typeof ships;
-  row: number;
-  col: number;
+  ship: keyof typeof SHIPS;
+  row?: number;
+  col?: number;
+  x?: number;
+  y?: number;
+  anchor?: number;
 }
 
-const ships = {
-  aircraftCarrier: {
-    path: import.meta.resolve("./assets/ships/aircraft-carrier2.png"),
-    rowspan: 4,
-    colspan: 2,
-  },
-  warship: {
-    path: import.meta.resolve("./assets/ships/warship2.png"),
-    rowspan: 5,
-    colspan: 1,
-  },
-};
-
 export default function ShipSprites(props: ShipSpritesProps) {
-  const { ship: shipName, row, col } = props;
-  const ship = ships[shipName];
+  const { ship: shipName, row, col, x, y, anchor } = props;
+  const ship = SHIPS[shipName];
+
+  const app = useApp();
+  const dragRef = useRef(null);
+
+  const onDragMove = (ev) => {
+    if (dragRef.current !== null) {
+      const sprite = dragRef.current;
+
+      // get mouse cursor pos
+      const currentPt = ev.global;
+      // calculate the ship center point
+      currentPt.x -= (ship.rowspan * GRID_SIZE) / 2;
+      currentPt.y -= (ship.colspan * GRID_SIZE) / 2;
+
+      sprite.parent.toLocal(currentPt, null, sprite.position);
+    }
+  };
+
+  const onDragEnd = () => {
+    if (dragRef.current !== null) {
+      const sprite = dragRef.current;
+      sprite.alpha = 1;
+      dragRef.current = null;
+
+      app.stage.off("pointermove", onDragMove);
+      app.stage.off("pointerup", onDragEnd);
+      app.stage.off("pointerupoutside", onDragEnd);
+    }
+  };
+
+  const onDragStart = (ev: PIXI.FederatedEvent) => {
+    const sprite = ev.target;
+    sprite.alpha = 0.5;
+    dragRef.current = sprite;
+
+    app.stage.on("pointermove", onDragMove);
+    app.stage.on("pointerup", onDragEnd);
+    app.stage.on("pointerupoutside", onDragEnd);
+  };
 
   return (
     <Sprite
       image={ship.path}
       height={GRID_SIZE * ship.colspan}
       width={GRID_SIZE * ship.rowspan}
-      x={GRID_SIZE * col}
-      y={GRID_SIZE * row}
+      x={col ? GRID_SIZE * col : x || 0}
+      y={row ? GRID_SIZE * row : y || 0}
+      anchor={anchor || 0}
+      eventMode="static"
       blendMode={PIXI.BLEND_MODES.OVERLAY}
+      onpointerdown={onDragStart}
     />
   );
 }

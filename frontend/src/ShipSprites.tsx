457be3e8cae37af4be4df8d35742f11083987705
Jimmy Chu <jimmychu0807@gmail.com>
2023-09-19 13:48:48 +0800

    Can snap a ship onto the board

--- frontend/src/ShipSprites.tsx
+++ frontend/src/ShipSprites.tsx
@@ -4,6 +4,7 @@ import * as PIXI from "pixi.js";
 
 import { GRID_SIZE, SHIPS } from "./consts";
 import { AppContext } from "./AppContext";
+import { snapShipToGrid } from "./utils";
 
 interface ShipSpritesProps {
   ship: keyof typeof SHIPS;
@@ -23,55 +24,57 @@ export default function ShipSprites(props: ShipSpritesProps) {
   const appContext = useContext(AppContext);
 
   const onDragMove = (ev: PIXI.FederatedMouseEvent) => {
-    if (!dragRef.current || !appContext) return;
+    if (!dragRef.current) return;
 
     const sprite = dragRef.current as PIXI.Sprite;
 
     // get mouse cursor pos
     const cursorPos = ev.global as PIXI.Point;
     // calculate the ship center point
-    const shipPos = cursorPos.clone();
-    shipPos.x -= (ship.rowspan * GRID_SIZE) / 2;
-    shipPos.y -= (ship.colspan * GRID_SIZE) / 2;
-
+    const shipPos = {
+      x: cursorPos.x - (ship.colspan * GRID_SIZE) / 2,
+      y: cursorPos.y - (ship.rowspan * GRID_SIZE) / 2,
+    };
     sprite.parent.toLocal(shipPos, undefined, sprite.position);
+  };
+
+  const onDragEnd = (ev: PIXI.FederatedMouseEvent) => {
+    if (!dragRef.current || !appContext) return;
+
+    // Remove the listeners
+    app.stage.off("pointermove", onDragMove);
+    app.stage.off("pointerup", onDragEnd);
+    app.stage.off("pointerupoutside", onDragEnd);
+
+    const sprite = dragRef.current as PIXI.Sprite;
+    dragRef.current = undefined;
 
     const board = appContext.appState?.board;
     if (!board) return;
 
-    const boardBound = board.getLocalBounds();
-    const boardPos = board.toGlobal(new PIXI.Point(0, 0));
-    const boardRightBottom = {
-      x: boardPos.x + boardBound.width,
-      y: boardPos.y + boardBound.height,
-    };
+    // get mouse cursor pos
+    const cursorPos = ev.global as PIXI.Point;
 
-    if (
-      cursorPos.x > boardPos.x &&
-      cursorPos.x < boardRightBottom.x &&
-      cursorPos.y > boardPos.y &&
-      cursorPos.y < boardRightBottom.y
-    ) {
-      console.log("ship dragged within board region");
+    // If the mouse cursor is inside the board, snap the ship to the board grid
+    const snapPos = snapShipToGrid(board, cursorPos, ship);
+    if (snapPos) {
+      sprite.alpha = 1;
+      sprite.parent.toLocal(snapPos, undefined, sprite.position);
+    } else {
+      // otherwise remove it.
+      sprite.destroy();
     }
   };
 
-  const onDragEnd = () => {
-    if (dragRef.current === undefined) return;
-
-    const sprite = dragRef.current as PIXI.Sprite;
-    sprite.alpha = 1;
-    dragRef.current = undefined;
-
-    app.stage.off("pointermove", onDragMove);
-    app.stage.off("pointerup", onDragEnd);
-    app.stage.off("pointerupoutside", onDragEnd);
-  };
-
   const onDragStart = (ev: PIXI.FederatedMouseEvent) => {
+    const clone = PIXI.Sprite.from(ship.path);
+    clone.alpha = 0.5;
+    clone.height = GRID_SIZE * ship.rowspan;
+    clone.width = GRID_SIZE * ship.colspan;
+
+    dragRef.current = clone;
     const sprite = ev.target as PIXI.Sprite;
-    sprite.alpha = 0.5;
-    dragRef.current = sprite;
+    sprite.parent.addChild(clone);
 
     app.stage.on("pointermove", onDragMove);
     app.stage.on("pointerup", onDragEnd);
@@ -81,8 +84,8 @@ export default function ShipSprites(props: ShipSpritesProps) {
   return (
     <Sprite
       image={ship.path}
-      height={GRID_SIZE * ship.colspan}
-      width={GRID_SIZE * ship.rowspan}
+      height={GRID_SIZE * ship.rowspan}
+      width={GRID_SIZE * ship.colspan}
       x={col ? GRID_SIZE * col : x || 0}
       y={row ? GRID_SIZE * row : y || 0}
       anchor={anchor || 0}

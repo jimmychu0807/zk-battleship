5a701d482a6d452758dd066b92b30e695d315b8b
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-04-26 22:41:29 +0800

    added one test

--- apps/hardhat/test/Battleship.ts
+++ apps/hardhat/test/Battleship.ts
@@ -1,6 +1,6 @@
-import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import hre from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 
 enum GameState {
   P1Joined = 0,
@@ -20,11 +20,26 @@ describe("Battleship", function () {
     return { battleship, p1, p2 };
   }
 
-  describe("Deployment", () => {
-    it("Should deploy a contract properly", async () => {
-      const { battleship, p1, p2 } = await loadFixture(deployFixture);
+  describe("Player 1 deploy the contract", () => {
+    it("should deploy a contract", async () => {
+      const { battleship, p1 } = await loadFixture(deployFixture);
       expect(await battleship.gameState()).equal(GameState.P1Joined);
       expect(await battleship.p1()).equal(await p1.getAddress());
+    });
+
+    it("should allow anyone who are not player 1 to join as another player", async () => {
+      const { battleship, p1, p2 } = await loadFixture(deployFixture);
+      const p2Addr = await p2.getAddress();
+
+      await expect(battleship.connect(p1).p2join())
+        .revertedWith(/same as player 1/);
+
+      await expect(battleship.connect(p2).p2join())
+        .emit(battleship, "P2Joined")
+        .withArgs(p2Addr);
+
+      expect(await battleship.p2()).equal(p2Addr);
+      expect(await battleship.gameState()).equal(GameState.P2Joined);
     });
   });
 });

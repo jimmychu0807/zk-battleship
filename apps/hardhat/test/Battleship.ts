8e51298a11736699bda7c16ba8a2e44abbf3f0a7
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-04-26 12:17:26 +0800

    fixing github CI action

--- apps/hardhat/test/Battleship.ts
+++ apps/hardhat/test/Battleship.ts
@@ -1,6 +1,6 @@
-import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers';
-import { expect } from 'chai';
-import hre from 'hardhat';
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import hre from "hardhat";
 
 enum GameState {
   P1Joined = 0,
@@ -11,17 +11,17 @@ enum GameState {
   P2Win,
 }
 
-describe('Battleship', function () {
+describe("Battleship", function () {
   async function deployFixture() {
     // @ts-expect-error
     const [p1, p2] = await hre.ethers.getSigners();
-    const Battleship = await hre.ethers.getContractFactory('Battleship');
+    const Battleship = await hre.ethers.getContractFactory("Battleship");
     const battleship = await Battleship.deploy();
     return { battleship, p1, p2 };
   }
 
-  describe('Deployment', () => {
-    it('Should deploy a contract properly', async () => {
+  describe("Deployment", () => {
+    it("Should deploy a contract properly", async () => {
       const { battleship, p1, p2 } = await loadFixture(deployFixture);
       expect(await battleship.game_state()).equal(GameState.P1Joined);
       expect(await battleship.p1()).equal(await p1.getAddress());

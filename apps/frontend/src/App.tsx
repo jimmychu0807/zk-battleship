2cae55e714997888886f33c9d9e2886137caa431
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-04-29 11:25:38 +0800

    Reset frontend with latest vite build (#20)
    * updated frontend
    
    * Basic frontend setup

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,52 +1,12 @@
-import { useEffect, useState } from "react";
-import { Stage, Text, useApp } from "@pixi/react";
-
-import { HeadingTextStyle, GRID_COLS, GRID_SIZE } from "./consts";
-import BoardGrid from "./BoardGrid";
-import { AppContext, AppState } from "./AppContext";
-import ShipPlate from "./ShipPlate";
-
-function Devtool() {
-  const app = useApp();
-  useEffect(() => {
-    // For PixiJS Devtools
-    // ref: https://github.com/bfanger/pixi-inspector#pixijs
-    // eslint-disable-next-line
-    (globalThis as any).__PIXI_APP__ = app;
-  }, [app]);
-
-  return <></>;
-}
+import "./App.css";
 
 function App() {
-  let y = 0;
-  const [appState, setAppState] = useState<AppState>({});
-
   return (
-    <Stage
-      options={{
-        antialias: true,
-        autoDensity: true,
-        backgroundColor: 0xeef1f5,
-        eventMode: "static",
-        resizeTo: window,
-      }}
-      width={window.innerWidth}
-      height={window.innerHeight}
-    >
-      <AppContext.Provider value={{ appState, setAppState }}>
-        <Devtool />
-        <Text text="Your board" x={0} y={y} style={HeadingTextStyle} />
-        <BoardGrid x={0} y={(y += 50)} />
-        <Text
-          text="Your ships (pick 5 below):"
-          x={0}
-          y={(y += GRID_COLS * GRID_SIZE + 10)}
-          style={HeadingTextStyle}
-        />
-        <ShipPlate x={0} y={(y += 50)} />
-      </AppContext.Provider>
-    </Stage>
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   );
 }
 

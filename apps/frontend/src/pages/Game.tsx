b56c351570b6083f76c0cfd5edfd5333011f7095
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-05-10 12:49:27 +0800

    Updated

--- apps/frontend/src/pages/Game.tsx
+++ apps/frontend/src/pages/Game.tsx
@@ -1,7 +1,8 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Flex, Text } from "@chakra-ui/react";
-import { useReadContracts } from "wagmi";
-
+import { useConfig, useReadContracts } from "wagmi";
+import { readContracts } from "wagmi/actions";
 import { battleshipArtifact, GameState } from "../helpers";
 
 async function loader({ params }) {
@@ -34,7 +35,85 @@ export default function Game() {
       <Text>Game status: {GameState[gameState.result]}</Text>
       <Text>Player 1: ${p1Addr.result}</Text>
       <Text>Player 2: ${p2Addr.result}</Text>
+      {GameState[gameState.result] === "P2Joined" && (
+        <SetupShips contractAddr={address} />
+      )}
+      {(GameState[gameState.result] === "P1Move" ||
+        GameState[gameState.result] === "P2Move") && (
+        <PlayerMove contractAddr={address} />
+      )}
+      {(GameState[gameState.result] === "P1Won" ||
+        GameState[gameState.result] === "P2Won") && (
+        <GameFinished contractAddr={address} />
+      )}
     </Flex>
+  );
+}
+
+function SetupShips({ contractAddr }) {
+  const wagmiConfig = useConfig();
+
+  const boardInfo = ["TOTAL_SHIPS", "BOARD_ROWS", "BOARD_COLS"];
+  const [address, abi] = [contractAddr, battleshipArtifact.abi];
+  const bResult = useReadContracts({
+    contracts: boardInfo.map((functionName) => ({
+      address,
+      abi,
+      functionName,
+    })),
+  });
+
+  const [shipInfo, setShipInfo] = useState([]);
+
+  const [totalShips, boardRows, boardCols] = bResult.data || [];
+
+  useEffect(() => {
+    async function getShipsInfo() {
+      // const shipInfo = ['SHIP_NAMES', 'SHIP_SIZES'];
+      if (!totalShips || !totalShips.result || totalShips.result === 0) return;
+
+      const totalShipsVal = totalShips.result;
+      const arr = Array(totalShipsVal)
+        .fill("")
+        .map((_, idx) => idx);
+      const sResult = await readContracts(wagmiConfig, {
+        contracts: arr.map((idx) => ({
+          address,
+          abi,
+          functionName: "SHIP_NAMES",
+          args: [idx],
+        })),
+      });
+
+      setShipInfo(sResult.map((res) => res.result));
+    }
+
+    getShipsInfo();
+  }, [totalShips, abi, address, wagmiConfig]);
+
+  console.log("boardInfo:", totalShips, boardRows, boardCols);
+  console.log("shipInfo:", shipInfo);
+
+  return (
+    <>
+      <Text>Setup Ships</Text>
+    </>
+  );
+}
+
+function PlayerMove() {
+  return (
+    <>
+      <Text>Player Move</Text>
+    </>
+  );
+}
+
+function GameFinished() {
+  return (
+    <>
+      <Text>Game Finished</Text>
+    </>
   );
 }
 

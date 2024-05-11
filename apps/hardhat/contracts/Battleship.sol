dce0e675d3c597538fb022d84236385940648b48
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-05-11 17:43:26 +0800

    updated contract to support multiple rounds in one contract

--- apps/hardhat/contracts/Battleship.sol
+++ apps/hardhat/contracts/Battleship.sol
@@ -52,8 +52,13 @@ contract Battleship is Ownable {
   event Hit(uint indexed roundId, address indexed opponent);
   event SinkShip(uint indexed roundId, address indexed opponent, uint8 shipIdx);
 
+  modifier validRoundId(uint roundId) {
+    require(roundId < nextRoundId, "roundId is out of bound");
+    _;
+  }
+
   modifier onlyPlayers(uint roundId) {
-    GameRound memory round = rounds[roundId];
+    GameRound storage round = rounds[roundId];
     require(msg.sender == round.p1 || msg.sender == round.p2, "Not one of the game players");
     _;
   }
@@ -63,8 +68,8 @@ contract Battleship is Ownable {
     _;
   }
 
-  modifier PlayerToMove(uint roundId) {
-    GameRound memory round = rounds[roundId];
+  modifier playerToMove(uint roundId) {
+    GameRound storage round = rounds[roundId];
     require(
       round.state == GameState.P1Move || round.state == GameState.P2Move,
       "Current game doesn't allow either players to move"
@@ -78,7 +83,7 @@ contract Battleship is Ownable {
     _;
   }
 
-  constructor() {
+  constructor() Ownable(msg.sender) {
     // Initialize ship types
     shipTypes.push(ShipType("Submarine", [1, 2]));
     shipTypes.push(ShipType("Cruiser", [1, 3]));
@@ -100,22 +105,40 @@ contract Battleship is Ownable {
   }
 
   function newGame() public {
-    rounds.push(GameRound({
-      p1: msg.sender,
-      p2: address(0),
-      state: GameState.P1Joined
-    }));
-
+    GameRound storage round = rounds.push();
+    round.p1 = msg.sender;
+    round.state = GameState.P1Joined;
     emit NewGame(nextRoundId, msg.sender);
 
     nextRoundId++;
   }
 
-  function getGameRounds() public view returns(GameRound[] memory) {
-    return rounds;
+  function getRoundInfo(uint roundId) public view
+    validRoundId(roundId)
+    returns(address, address, GameState)
+  {
+    GameRound storage round = rounds[roundId];
+    return (round.p1, round.p2, round.state);
   }
 
-  function p2join(uint roundId) public allowedState(roundId, GameState.P1Joined) {
+  function getRoundMoves(uint roundId, address p) public view
+    validRoundId(roundId)
+    returns(uint8[2][] memory)
+  {
+    return rounds[roundId].moves[p];
+  }
+
+  function getRoundShips(uint roundId, address p) public view
+    validRoundId(roundId)
+    returns(Ship[] memory)
+  {
+    return rounds[roundId].ships[p];
+  }
+
+  function p2join(uint roundId) public
+    validRoundId(roundId)
+    allowedState(roundId, GameState.P1Joined)
+  {
     GameRound storage round = rounds[roundId];
     require(round.p1 != msg.sender, "Cannot be the same as player 1");
     round.p2 = msg.sender;
@@ -130,6 +153,7 @@ contract Battleship is Ownable {
     uint8[2] memory topLeft,
     uint8[2] memory bottomRight
   ) public
+    validRoundId(roundId)
     allowedState(roundId, GameState.P2Joined)
     onlyPlayers(roundId)
   {
@@ -176,10 +200,13 @@ contract Battleship is Ownable {
     emit SetupShip(roundId, msg.sender, shipId);
   }
 
-  function startGame(uint roundId) public allowedState(roundId, GameState.P2Joined) {
+  function startGame(uint roundId) public
+    validRoundId(roundId)
+    allowedState(roundId, GameState.P2Joined)
+  {
     GameRound storage round = rounds[roundId];
-    address memory p1 = round.p1;
-    address memory p2 = round.p2;
+    address p1 = round.p1;
+    address p2 = round.p2;
 
     require(round.ships[p1].length == getShipTypeNum(), "player 1 ships are not properly setup");
     require(round.ships[p2].length == getShipTypeNum(), "player 2 ships are not properly setup");
@@ -195,18 +222,21 @@ contract Battleship is Ownable {
     emit GameStart(roundId);
   }
 
-  // NX: get up to here
-
-  function playerMove(uint8[2] memory hitRC) public PlayerToMove {
+  function playerMove(uint roundId, uint8[2] memory hitRC) public
+    validRoundId(roundId)
+    playerToMove(roundId)
+  {
     // Logic of adding the move in the corresponding move list
     require(hitRC[0] < boardSize[0] && hitRC[1] < boardSize[1], "Player move is out of bound");
 
     // Add to the player move list
-    uint8[2][] storage playerMoves = moves[msg.sender];
+    GameRound storage round = rounds[roundId];
+    uint8[2][] storage playerMoves = round.moves[msg.sender];
     playerMoves.push(hitRC);
 
     // Check if it hits opponent ship
-    Ship[] storage opponentShips = msg.sender == p1 ? ships[p2] : ships[p1];
+    address opponent = msg.sender == round.p1 ? round.p2 : round.p1;
+    Ship[] storage opponentShips = round.ships[opponent];
 
     for (uint8 sIdx = 0; sIdx < getShipTypeNum(); sIdx++) {
       Ship storage ship = opponentShips[sIdx];
@@ -217,49 +247,53 @@ contract Battleship is Ownable {
         // hit the ship
         uint8 rowWidth = ship.bottomRight[1] - ship.topLeft[1] + 1;
         uint8 bodyIdx = (hitRC[0] - ship.topLeft[0]) * rowWidth + (hitRC[1] - ship.topLeft[1]);
-        uint8 reverseIdx = (shipType[sIdx].size[0] * shipType[sIdx].size[1]) - bodyIdx - 1;
+        uint8 reverseIdx = (shipTypes[sIdx].size[0] * shipTypes[sIdx].size[1]) - bodyIdx - 1;
         uint256 mask = ~(1 << reverseIdx);
         ship.body &= mask;
 
         // mark the ship as dead if its body are all hit
-        address opponent = msg.sender == p1 ? p2 : p1;
         if (ship.body > 0) {
-          emit Hit(opponent);
+          emit Hit(roundId, opponent);
         } else {
           ship.alive = false;
-          emit SinkShip(opponent, sIdx);
+          emit SinkShip(roundId, opponent, sIdx);
         }
       }
     }
 
     // Update the game state
-    gameState = isGameEnd()
-      ? (gameState == GameState.P1Move ? GameState.P1Won : GameState.P2Won)
-      : (gameState == GameState.P1Move ? GameState.P2Move : GameState.P1Move);
+    round.state = isGameEnd(roundId)
+      ? (round.state == GameState.P1Move ? GameState.P1Won : GameState.P2Won)
+      : (round.state == GameState.P1Move ? GameState.P2Move : GameState.P1Move);
 
     // Emit an event
-    emit PlayerMove(msg.sender, hitRC, gameState);
+    emit PlayerMove(roundId, msg.sender, hitRC, round.state);
   }
 
-  function isGameEnd() public view returns (bool) {
+  function isGameEnd(uint roundId) public view
+    validRoundId(roundId)
+    returns (bool)
+  {
     // check if the game ends
-    Ship[] storage p1ships = ships[p1];
-    Ship[] storage p2ships = ships[p2];
+    GameRound storage round = rounds[roundId];
 
-    if (gameState == GameState.P1Joined || gameState == GameState.P2Joined)
+    if (round.state == GameState.P1Joined || round.state == GameState.P2Joined)
       return false;
 
-    if (gameState == GameState.P1Won || gameState == GameState.P2Won)
+    if (round.state == GameState.P1Won || round.state == GameState.P2Won)
       return true;
 
     bool bEnd = true;
+    Ship[] memory p1ships = round.ships[round.p1];
+    Ship[] memory p2ships = round.ships[round.p2];
+
     for (uint8 s = 0; s < getShipTypeNum(); s++) {
       if (p1ships[s].alive) {
         bEnd = false;
         break;
       }
     }
-    if (bEnd) return bEnd;
+    if (bEnd) return true;
 
     bEnd = true;
     for (uint8 s = 0; s < getShipTypeNum(); s++) {

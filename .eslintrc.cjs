bb1e32de1cf3c3f7c3071d557a6d5bca92b3c895
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-05-22 13:08:54 +0800

    renamed dirs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "apps/hardhat/artifacts"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "apps/*/artifacts"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
